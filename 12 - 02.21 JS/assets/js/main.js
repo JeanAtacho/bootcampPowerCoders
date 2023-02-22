cb890e9dee9e878d455ce387143860a082360390
jeanAtacho <ing.jeanatacho@gmail.com>
2023-02-22 14:59:15 +0100

    1

---
+++ 12 - 02.21 JS/assets/js/main.js
@@ -0,0 +1,19 @@
+// Let: variable no tan global.
+// var: prefiere no usarla, pero sirve para 
+
+//var hola = 'Hola Mundo!'
+
+//console.log(hola);
+
+const productos = 
+    { nombre: 'Azucar', narca: 'Blanca', Precio: 2 };
+    { nombre: 'Harina', narca: 'P.A.N', Precio: 2.5 };
+    { nombre: 'Cafe descafeinado', narca: 'Dona Juana', Precio: 3 };
+}
+let arr = 'Sal';
+console.log({ arr, productos })
+
+
+
+
+
