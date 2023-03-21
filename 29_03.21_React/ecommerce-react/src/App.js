959e00c569f4a1e5d1ebf51419bbf0436a006531
jeanAtacho <ing.jeanatacho@gmail.com>
2023-03-21 16:18:18 +0100

    2

--- 29_03.21_React/ecommerce-react/src/App.js
+++ 29_03.21_React/ecommerce-react/src/App.js
@@ -1,5 +1,14 @@
-function App(params) {
-  return ''
+import { useState } from 'React'
+import Products from './components/Products.jsx'
+import productsList from './products.json'
+
+function App() {
+  const [products] = useState(productsList)
+  
+  return <div class="app">
+    <h1 className="app_title">Trending Products</h1>
+    <Products data={ products } />
+  </div>
 }
 
 export default App
---
+++ 29_03.21_React/ecommerce-react/src/components/Products.jsx
@@ -0,0 +1,10 @@
+import Product from "./Product.jsx"
+
+function Products(props) {
+  return <ul className="products">
+    <Product />
+  </ul>
+}
+
+export default Products
+
