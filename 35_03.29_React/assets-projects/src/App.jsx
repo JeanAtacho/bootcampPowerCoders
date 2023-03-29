41627f5a1d0512921ed7b5c79ac515560a5524f7
jeanAtacho <ing.jeanatacho@gmail.com>
2023-03-29 16:37:01 +0200

    222

---
+++ 35_03.29_React/assets-projects/src/App.jsx
@@ -0,0 +1,12 @@
+import {square} from './App.module.css'
+
+function App() {
+
+  return (
+    <div>
+    <h1>Esto es una importacion del archivo css</h1>
+    <div className={square}></div>
+    </div>
+  )
+}
+export default App
