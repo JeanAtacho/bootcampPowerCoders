17758b4e0cc542073616e2ba38a2b9cd93e07385
jeanAtacho <ing.jeanatacho@gmail.com>
2023-03-29 17:34:33 +0200

    iiii

--- 35_03.29_React/assets-projects/src/App.jsx
+++ 35_03.29_React/assets-projects/src/App.jsx
@@ -1,11 +1,12 @@
-import {square} from './App.module.css'
+import { square, image } from './App.module.css'
 
 function App() {
-
   return (
-    <div>
-    <h1>Esto es una importacion del archivo css</h1>
-    <div className={square}></div>
+    <div >
+      <h1>Esto es una importacion del archivo css</h1>
+      <div className={square} >
+        <img src='./' className={image} />
+      </div>
     </div>
   )
 }
